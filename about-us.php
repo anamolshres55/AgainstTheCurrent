f544ee0b6292ecd89fca33d6f9912d06259c3843
Anmol <anamolshres55@gmail.com>
2020-01-13 18:42:24 +0545

    added files to finished project

---
+++ about-us.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Dosis Font -->
+    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+
+    <!-- PT-Sans Font -->
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+
+    <!-- FontAwesome Icons -->
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- Custom JS  -->
+    <link rel="stylesheet" href="js/custom.js">
+
+
+
+    <title>Against The Current - About Us </title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <a href="#" class="navbar-brand"><img src="img/logo.png" class="navbar-logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a href="index.php" class="menu-item">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about-us.php" class="menu-item">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="menu-item">Tours </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="menu-item">Merchs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="menu-item">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <div class="about-container">
+        <div class="black-overlay"></div>
+        <div class="container about-section">
+            <div class="row">
+                <div class="col-md-8 col-sm-12  about-info">
+                    <div class="about-info-title">
+                        <h3> Against The Current (ATC) </h3>
+                    </div>
+                    <p>
+                        Against The Current (often abbreviated as ATC) is an American pop rock band based in
+                        Poughkeepsie,
+                        New York, and formed in 2011. The band currently consists of lead vocalist Chrissy Costanza,
+                        guitarist Dan Gow, and drummer Will Ferri. The group gained a sizable online following after
+                        posting covers of various popular songs on YouTube.
+
+                        The band's first EP, Infinity, was released in May 2014 under their own label. The band
+                        followed
+                        Infinity with their next EP, Gravity, which was released on February 17, 2015. Shortly
+                        afterward,
+                        they signed to the record label Fueled by Ramen. Their debut full-length album, In Our Bones
+                        was
+                        released on May 20, 2016. Their second full-length album, "Past Lives" is due for release on
+                        September 28, 2018.
+                    </p>
+                    <p>
+                        Against the Current was formed in early 2011 by Dan Gow, Will Ferri, Joe Simmons, and Jeremy
+                        Rompala.
+                        Current lead singer Chrissy Costanza later joined the band in the summer of 2011 after being
+                        introduced to
+                        Gow, Ferri, and Rompala through a mutual friend.
+
+                        The group achieved a considerable amount of fame on YouTube after posting covers of various
+                        songs while
+                        working with other well known YouTube cover artists, such as Alex Goot and Sam Tsui.
+                    </p>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <img src="img/wallpaper6.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <footer>
+        <div class="footer-top-line"></div>
+        <div class="text-center footer-social-icons">
+            <a href="#" class="footer-social-icon "><i class="fab fa-facebook"></i></a>
+            <a href="#" class="footer-social-icon "><i class="fab fa-twitter"></i></a>
+            <a href="#" class="footer-social-icon "><i class="fab fa-instagram"></i></a>
+            <a href="#" class="footer-social-icon "><i class="fab fa-youtube"></i></a>
+            <a href="#" class="footer-social-icon "><i class="fab fa-spotify"></i></a>
+            <a href="#" class="footer-social-icon "><i class="fab fa-apple"></i></a>
+        </div>
+        <div class="footer-copyright">
+            <p> © ANMOL SHRESTHA , ALL RIGHTS RESERVED </p>
+        </div>
+        <div class="footer-bottom-line">
+
+        </div>
+    </footer>
+</body>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
